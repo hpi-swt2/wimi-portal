79e4d9e65d8f3a3b797276c550fe72724fa50e20
tymolc <tim.oesterreich@student.hpi.de>
2015-11-17 13:03:00 +0100

    requestHoliday button added

--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -1,3 +1,3 @@
 class Holiday < ActiveRecord::Base
-  belongs_to :users
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :holiday
-  has_many :expense
-  has_many :trip
+  has_many :holidayds
+  has_many :expenses
+  has_many :trips
   has_and_belongs_to_many :publications
   has_and_belongs_to_many :projects
 
