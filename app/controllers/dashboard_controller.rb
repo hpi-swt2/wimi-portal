df627f38a91d51e715482a42b68899ff1e4309a8
Toni Stachewicz <toni.stachewicz@student.hpi.de>
2015-11-23 13:36:34 +0100

    better solution for showing projects, test fixed

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,4 @@
 class DashboardController < ApplicationController
   def index
-    @user = current_user
   end
 end
--- spec/views/dashboard/index.html.erb_spec.rb
+++ spec/views/dashboard/index.html.erb_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'dashboard/index.html.erb', type: :view do
   it 'displays the projects of the user' do
-  	expect(1).to eq(5-4)
-  	#render :template => 'dashboard/index.html.erb'
-  	#expect(rendered).to have_content 'My projects'
-  	#expect(page).to have_table('table table-striped')
+    user = FactoryGirl.create(:user)
+    sign_in user
+    render
+    expect(rendered).to have_content('My Projects')
   end
 end
