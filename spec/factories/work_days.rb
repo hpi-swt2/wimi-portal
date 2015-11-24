200e516f65127aae138700b283d64c22af37ddc3
Moritz Eissenhauer <moritz.eissenhauer@gmail.com>
2015-11-24 21:47:36 +0100

    removed some auto generated specs

--- spec/factories/work_days.rb
+++ spec/factories/work_days.rb
@@ -2,11 +2,10 @@ FactoryGirl.define do
   factory :work_day do
     date "2015-11-18"
 start_time "2015-11-18 15:11:53"
-brake 1
-end_time "2015-11-18 15:11:53"
-duration 1
-attendance "MyString"
-notes "MyString"
+brake 30
+end_time "2015-11-18 16:11:53"
+attendance ""
+notes "some note"
   end
 
 end
--- spec/factories/work_months.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :work_month do
-    name "MyString"
-year 1
-  end
-
-end
