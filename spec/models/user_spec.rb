ff485a1daf3d83944c478916e89bd67b1f348f92
Theresa <theresa.zobel@student.hpi.de>
2015-11-24 20:35:40 +0100

    Fix navbar size and container position

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,24 +2,18 @@
 #
 # Table name: users
 #
-#  id                     :integer          not null, primary key
-#  email                  :string           default(""), not null
-#  encrypted_password     :string           default(""), not null
-#  reset_password_token   :string
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  sign_in_count          :integer          default(0), not null
-#  current_sign_in_at     :datetime
-#  last_sign_in_at        :datetime
-#  current_sign_in_ip     :string
-#  last_sign_in_ip        :string
-#  first                  :string
-#  last_name              :string
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
-#  identity_url           :string
-#  username               :string
-#  token                  :string
+#  id                 :integer          not null, primary key
+#  email              :string           default(""), not null
+#  sign_in_count      :integer          default(0), not null
+#  current_sign_in_at :datetime
+#  last_sign_in_at    :datetime
+#  current_sign_in_ip :string
+#  last_sign_in_ip    :string
+#  first              :string
+#  last_name          :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  identity_url       :string
 #
 
 require 'rails_helper'
