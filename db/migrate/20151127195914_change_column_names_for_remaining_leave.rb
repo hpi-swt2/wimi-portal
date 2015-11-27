10aed66ea7f8a1e1a4eb96536e59ddd58f1ade20
tymolc <tim.oesterreich@student.hpi.de>
2015-11-27 21:45:10 +0100

    changed faulty migration

--- db/migrate/20151127195914_change_column_names_for_remaining_leave.rb
+++ db/migrate/20151127195914_change_column_names_for_remaining_leave.rb
@@ -1,7 +1,5 @@
 class ChangeColumnNamesForRemainingLeave < ActiveRecord::Migration
   def change
-  	remove_column :holidays, :remaining_leave
-  	remove_column :holidays, :remaining_leave_last_year
   	remove_column :users, :remaining_leave_this_year
   	remove_column :users, :remaining_leave_next_year
   	add_column :users, :remaining_leave, :integer, default: 28
