b21818bd1be06f959c5fb9916112c6889ab8f9fc
Lennard Wolf <lennardwolf@live.de>
2015-11-28 16:14:46 +0100

    add redirection when missing parameters in GET work_days request #no_failures

--- spec/requests/work_days_spec.rb
+++ spec/requests/work_days_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "WorkDays", type: :request do
   describe "GET /work_days" do
     it "works! (now write some real specs)" do
       get work_days_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(302)
     end
   end
 end
