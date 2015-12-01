1857f0b3a8c71c4a6282ba54c01428cbeb824f74
Theresa <theresa.zobel@student.hpi.de>
2015-12-01 20:31:30 +0100

    Add status and public label and radio boxes for them

--- db/migrate/20151006150309_create_projects.rb
+++ db/migrate/20151006150309_create_projects.rb
@@ -2,6 +2,9 @@ class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
       t.string :title
+      t.boolean :status, default: true
+      t.boolean :public, default: true
+
       t.timestamps null: false
     end
   end
