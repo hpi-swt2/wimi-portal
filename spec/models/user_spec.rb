c4f384de0a4613822debed282197f6fdf80049d3
wgie <willi.gierke@futurice.com>
2015-12-01 15:28:46 +0100

    added annotations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                        :integer          not null, primary key
+#  email                     :string           default(""), not null
+#  sign_in_count             :integer          default(0), not null
+#  current_sign_in_at        :datetime
+#  last_sign_in_at           :datetime
+#  current_sign_in_ip        :string
+#  last_sign_in_ip           :string
+#  first                     :string
+#  last_name                 :string
+#  created_at                :datetime         not null
+#  updated_at                :datetime         not null
+#  identity_url              :string
+#  remaining_leave_this_year :integer          default(28)
+#  remaining_leave_next_year :integer          default(28)
+#
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
