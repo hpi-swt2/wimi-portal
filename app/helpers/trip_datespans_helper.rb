a0d34aa7c350b5dfaca853f4c74e5abc77959281
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2015-12-07 10:55:29 +0100

    add trip_datespan model

---
+++ app/helpers/trip_datespans_helper.rb
@@ -0,0 +1,2 @@
+module TripDatespansHelper
+end
---
+++ spec/factories/trip_datespans.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :trip_datespan do
+    start_date "2015-12-07"
+end_date "2015-12-07"
+days_abroad 1
+trip nil
+  end
+
+end
