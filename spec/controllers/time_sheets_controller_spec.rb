3bb292885cecacaedddb4c32c629ee445968fadb
Jan van Dieken <Zanthelo@users.noreply.github.com>
2015-12-08 20:05:34 +0100

    added tests and removed unused functions

--- spec/controllers/time_sheets_controller_spec.rb
+++ spec/controllers/time_sheets_controller_spec.rb
@@ -29,13 +29,15 @@ RSpec.describe TimeSheetsController, type: :controller do
   # TimeSheet. As you add validations to TimeSheet, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    {month:1, year:2015, salary:100, salary_is_per_month:false,
-      workload:100, workload_is_per_month: true, user_id:@user.id,
-      project_id:@project.id}
+    { month: 1, year: 2015, salary: 100, salary_is_per_month: false,
+      workload: 100, workload_is_per_month: true, user_id: @user.id,
+      project_id: @project.id }
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    { month:1, year: 2015, salary: -100, salary_is_per_month: false,
+      workload: 100, workload_is_per_month: true, user_id: @user.id,
+      project_id: @project.id }
   }
 
   # This should return the minimal set of values that should be in the session
@@ -50,4 +52,48 @@ RSpec.describe TimeSheetsController, type: :controller do
       expect(assigns(:time_sheet)).to eq(time_sheet)
     end
   end
+
+  describe 'PUT #update' do
+    context 'with valid params' do
+      let(:new_attributes) {
+        { month: 1, year: 2015, salary: 100, salary_is_per_month: false,
+          workload: 200, workload_is_per_month: true, user_id: @user.id,
+          project_id: @project.id }
+      }
+
+      it 'updates the requested time_sheet' do
+        time_sheet = TimeSheet.create! valid_attributes
+        put :update, {id: time_sheet.to_param, time_sheet: new_attributes}, valid_session
+        time_sheet.reload
+        expect(time_sheet.workload).to eq(200)
+      end
+
+      it 'assigns the requested time_sheet as @time_sheet' do
+        time_sheet = TimeSheet.create! valid_attributes
+        put :update, {id: time_sheet.to_param, time_sheet: valid_attributes}, valid_session
+        expect(assigns(:time_sheet)).to eq(time_sheet)
+      end
+
+      it 'redirects to the time_sheet' do
+        time_sheet = TimeSheet.create! valid_attributes
+        put :update, {id: time_sheet.to_param, time_sheet: valid_attributes}, valid_session
+        expect(response).to redirect_to(work_days_path(month: time_sheet.month, year: time_sheet.year, project: time_sheet.project))
+      end
+    end
+
+    context 'with invalid params' do
+      it 'assigns the time_sheet as @time_sheet' do
+        time_sheet = TimeSheet.create! valid_attributes
+        put :update, {id: time_sheet.to_param, time_sheet: invalid_attributes}, valid_session
+        expect(assigns(:time_sheet)).to eq(time_sheet)
+      end
+
+      it "re-renders the 'edit' template" do
+        time_sheet = TimeSheet.create! valid_attributes
+        put :update, {id: time_sheet.to_param, time_sheet: invalid_attributes}, valid_session
+        expect(response).to render_template('edit')
+      end
+    end
+  end
+
 end
