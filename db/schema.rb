40a77cb311949e6cc35e3072a376fcfc66b4fc41
wgie <willi.gierke@futurice.com>
2015-12-09 16:57:57 +0100

    added missing updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,13 @@ ActiveRecord::Schema.define(version: 20151127195914) do
 
   add_index "holidays", ["user_id"], name: "index_holidays_on_user_id"
 
+  create_table "notifications", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "projects", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
