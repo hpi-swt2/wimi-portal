ec81c8e4a56567299ebb4ba669b1f20b6d2a9149
Niklas Hoffmann <niklas.hoffmann@student.hpi.de>
2015-12-09 16:53:20 +0100

    changed notation of seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 # define the account, which has privileges of a superadmin
-User.create(:first_name => 'First', :last_name => 'Last', :email => 'first.last@student.hpi.uni-potsdam.de', :identity_url => 'https://openid.hpi.uni-potsdam.de/user/first.last', :superadmin => true)
+User.create(first_name: 'First', last_name: 'Last', email: 'first.last@student.hpi.uni-potsdam.de', identity_url: 'https://openid.hpi.uni-potsdam.de/user/first.last', superadmin: true)
