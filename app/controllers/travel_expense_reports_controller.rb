035abbca0e0ac986344c2f622fb0313bd240ec7a
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2015-12-09 09:30:16 +0100

    clean controllers, remove scaffold coffee and scss

--- app/controllers/travel_expense_reports_controller.rb
+++ app/controllers/travel_expense_reports_controller.rb
@@ -1,30 +1,22 @@
 class TravelExpenseReportsController < ApplicationController
   before_action :set_travel_expense_report, only: [:show, :edit, :update, :destroy]
 
-  # GET /travel_expense_reports
-  # GET /travel_expense_reports.json
   def index
     @travel_expense_reports = TravelExpenseReport.all
   end
 
-  # GET /travel_expense_reports/1
-  # GET /travel_expense_reports/1.json
   def show
   end
 
-  # GET /travel_expense_reports/new
   def new
     @travel_expense_report = TravelExpenseReport.new
     @travel_expense_report.user = current_user
     8.times {@travel_expense_report.travel_expense_report_items.build}
   end
 
-  # GET /travel_expense_reports/1/edit
   def edit
   end
 
-  # POST /travel_expense_reports
-  # POST /travel_expense_reports.json
   def create
     @travel_expense_report = TravelExpenseReport.new(travel_expense_report_params)
     @travel_expense_report.user = current_user
@@ -32,45 +24,34 @@ class TravelExpenseReportsController < ApplicationController
     respond_to do |format|
       if @travel_expense_report.save
         format.html { redirect_to @travel_expense_report, notice: 'Travel expense report was successfully created.' }
-        format.json { render :show, status: :created, location: @travel_expense_report }
       else
         format.html { render :new }
-        format.json { render json: @travel_expense_report.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /travel_expense_reports/1
-  # PATCH/PUT /travel_expense_reports/1.json
   def update
     respond_to do |format|
       if @travel_expense_report.update(travel_expense_report_params)
         format.html { redirect_to @travel_expense_report, notice: 'Travel expense report was successfully updated.' }
-        format.json { render :show, status: :ok, location: @travel_expense_report }
       else
         format.html { render :edit }
-        format.json { render json: @travel_expense_report.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /travel_expense_reports/1
-  # DELETE /travel_expense_reports/1.json
   def destroy
     @travel_expense_report.destroy
     respond_to do |format|
       format.html { redirect_to travel_expense_reports_url, notice: 'Travel expense report was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_travel_expense_report
       @travel_expense_report = TravelExpenseReport.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def travel_expense_report_params
       params.require(:travel_expense_report).permit(:first_name, :last_name, :inland, :country, :location_from, :location_via, :location_to, :reason, :date_start, :date_end, :car, :public_transport, :vehicle_advance, :hotel, :general_advance, :user_id, travel_expense_report_items_attributes:[:date,:breakfast,:lunch,:dinner])
     end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,29 +1,21 @@
 class TripsController < ApplicationController
   before_action :set_trip, only: [:show, :edit, :update, :destroy, :download]
 
-  # GET /trips
-  # GET /trips.json
   def index
     @trips = Trip.all
   end
 
-  # GET /trips/1
-  # GET /trips/1.json
   def show
   end
 
-  # GET /trips/new
   def new
     @trip = Trip.new
     2.times {@trip.trip_datespans.build} 
   end
 
-  # GET /trips/1/edit
   def edit
   end
 
-  # POST /trips
-  # POST /trips.json
   def create
     @trip = Trip.new(trip_params)
     @trip.user = current_user
@@ -31,35 +23,26 @@ class TripsController < ApplicationController
     respond_to do |format|
       if @trip.save
         format.html { redirect_to @trip, notice: 'Trip was successfully created.' }
-        format.json { render :show, status: :created, location: @trip }
       else
         format.html { render :new }
-        format.json { render json: @trip.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /trips/1
-  # PATCH/PUT /trips/1.json
   def update
     respond_to do |format|
       if @trip.update(trip_params)
         format.html { redirect_to @trip, notice: 'Trip was successfully updated.' }
-        format.json { render :show, status: :ok, location: @trip }
       else
         format.html { render :edit }
-        format.json { render json: @trip.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /trips/1
-  # DELETE /trips/1.json
   def destroy
     @trip.destroy
     respond_to do |format|
       format.html { redirect_to trips_url, notice: 'Trip was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
@@ -67,12 +50,10 @@ class TripsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_trip
       @trip = Trip.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def trip_params
 	    params.require(:trip).permit(:name, :destination, :reason, :annotation, :signature, trip_datespans_attributes:[:start_date, :end_date,:days_abroad])
     end
