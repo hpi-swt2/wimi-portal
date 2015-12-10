a1cbf282e03a3ec4980977c2229586ce29a8f648
Moritz Eissenhauer <moritz.eissenhauer@gmail.com>
2015-12-10 13:17:52 +0100

    removes comments and json

--- app/controllers/time_sheets_controller.rb
+++ app/controllers/time_sheets_controller.rb
@@ -1,22 +1,16 @@
 class TimeSheetsController < ApplicationController
   before_action :set_time_sheet, only: [:show, :edit, :update, :destroy]
 
-  # GET /time_sheets/1
-  # GET /time_sheets/1.json
   def show
   end
 
-  # GET /time_sheets/new
   def new
     @time_sheet = TimeSheet.new
   end
 
-  # GET /time_sheets/1/edit
   def edit
   end
 
-  # PATCH/PUT /time_sheets/1
-  # PATCH/PUT /time_sheets/1.json
   def update
     if @time_sheet.update(time_sheet_params)
       redirect_to work_days_path(month: @time_sheet.month, year: @time_sheet.year, project: @time_sheet.project)
@@ -26,12 +20,11 @@ class TimeSheetsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_time_sheet
       @time_sheet = TimeSheet.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def time_sheet_params
       params.require(:time_sheet).permit(:month, :year, :salary, :salary_is_per_month, :workload, :workload_is_per_month, :user_id, :project_id)
     end
--- app/controllers/work_days_controller.rb
+++ app/controllers/work_days_controller.rb
@@ -1,8 +1,6 @@
 class WorkDaysController < ApplicationController
   before_action :set_work_day, only: [:show, :edit, :update, :destroy]
 
-  # GET /work_days
-  # GET /work_days.json
   def index
     if params.has_key?(:month) && params.has_key?(:year)
       @month = params[:month].to_i
@@ -16,60 +14,39 @@ class WorkDaysController < ApplicationController
     end
   end
 
-  # GET /work_days/1
-  # GET /work_days/1.json
   def show
   end
 
-  # GET /work_days/new
   def new
     @work_day = WorkDay.new
   end
 
-  # GET /work_days/1/edit
   def edit
   end
 
-  # POST /work_days
-  # POST /work_days.json
   def create
     @work_day = WorkDay.new(work_day_params)
     @work_day.user_id = current_user.id
 
-    respond_to do |format|
-      if @work_day.save
-        format.html { redirect_to work_days_month_path }
-        format.json { render :show, status: :created, location: @work_day }
-      else
-        format.html { render :new }
-        format.json { render json: @work_day.errors, status: :unprocessable_entity }
-      end
+    if @work_day.save
+      redirect_to work_days_month_path
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /work_days/1
-  # PATCH/PUT /work_days/1.json
   def update
-    respond_to do |format|
-      if @work_day.update(work_day_params)
-        format.html { redirect_to work_days_month_path }
-        format.json { render :show, status: :ok, location: @work_day }
-      else
-        format.html { render :edit }
-        format.json { render json: @work_day.errors, status: :unprocessable_entity }
-      end
+    if @work_day.update(work_day_params)
+      redirect_to work_days_month_path
+    else
+      render :edit
     end
   end
 
-  # DELETE /work_days/1
-  # DELETE /work_days/1.json
   def destroy
     date = @work_day.date
     @work_day.destroy
-    respond_to do |format|
-      format.html { redirect_to work_days_path(month: date.month, year: date.year) }
-      format.json { head :no_content }
-    end
+    redirect_to work_days_path(month: date.month, year: date.year)
   end
 
   private
@@ -78,7 +55,6 @@ class WorkDaysController < ApplicationController
       @work_day = WorkDay.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def work_day_params
       params.require(:work_day).permit(:date, :start_time, :break, :end_time, :duration, :attendance, :notes, :user_id, :project_id)
     end
