988faaf25733546fcd8530bc6e741f0186a9dbb4
Moritz Eissenhauer <moritz.eissenhauer@gmail.com>
2015-12-11 12:34:17 +0100

    validates the existance of the work_days project

--- app/models/work_day.rb
+++ app/models/work_day.rb
@@ -3,12 +3,18 @@ class WorkDay < ActiveRecord::Base
 	belongs_to :project
 
 	validates :user_id, presence: true, numericality: true
+	validates :project_id, presence:true, numericality: true
 	validates :date, presence: true
 	validates :start_time, presence: true
 	validates :break, presence: true, numericality: true
 	validates :end_time, presence: true
+	validate :project_id_exists
 
 	def duration
-        return (end_time - start_time).to_i / 60 - self.break
-    end
+    return (end_time - start_time).to_i / 60 - self.break
+  end
+
+	def project_id_exists
+		return false if Project.find_by_id(self.project_id).nil?
+	end
 end
