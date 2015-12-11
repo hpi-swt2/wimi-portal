4c168bf50b72c40996f36691963b201795cc72e2
Chaoste <t-kellermeier@hotmail.de>
2015-12-11 13:18:15 +0100

    Move stylesheet file for pdf generator into assets/stylesheets

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -5,8 +5,8 @@ class DocumentsController < ApplicationController
         raise ArgumentError, "doc_type or doc_id is not set (e.g. for doc_type 'Dienstreiseantrag')"
       else
         self.init_params
-        # WickedPDF looks for stylesheet files in Rails.root/public
-        @tmp_vars[:css_file] = "/../app/views/documents/styles.css"
+        # WickedPDF looks for stylesheet files in app/assets/stylesheets
+        @tmp_vars[:css_file] = "styles.css"
         @tmp_vars[:hpi_logo] = "#{Rails.root}/app/views/documents/img/HPI-logo.jpg"
 
         pdf = WickedPdf.new.pdf_from_string(render_to_string(
@@ -25,8 +25,14 @@ class DocumentsController < ApplicationController
         @tmp_vars[:trip] = Trip.find(params[:doc_id])
         @tmp_vars[:trip].annotation = @tmp_vars[:trip].annotation || ""
         @tmp_vars[:annotation_splitted] = @tmp_vars[:trip].annotation.split("\n")[0,4]
-      else
-        raise NotImplementedError
+      when 'Urlaubsantrag'
+          @tmp_vars[:holiday] = Holiday.find(params[:doc_id])
+          @tmp_vars[:holiday].annotation = @tmp_vars[:holiday].annotation || ""
+          @tmp_vars[:annotation_splitted] = @tmp_vars[:holiday].annotation.split("\n")
+        when 'Reisekostenabrechnung'
+          @tmp_vars[:report] = Travel_expenses_report.find(params[:doc_id])
+        else
+          raise NotImplementedError
       end
   end
 
