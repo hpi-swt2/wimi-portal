7ce5d0a2d8c41a70321351d706c74c38b6496c8d
Moritz Eissenhauer <moritz.eissenhauer@gmail.com>
2015-12-11 13:49:48 +0100

    moved time_sheet_for into TimeSheet model

--- app/controllers/work_days_controller.rb
+++ app/controllers/work_days_controller.rb
@@ -6,7 +6,7 @@ class WorkDaysController < ApplicationController
       @month = params[:month].to_i
       @year = params[:year].to_i
       @project = params.has_key?(:project) ? Project.find(params[:project].to_i) : nil
-      @time_sheet = time_sheet_for(@year, @month, @project)
+      @time_sheet = TimeSheet.time_sheet_for(@year, @month, @project, current_user)
       @work_days = WorkDay.all_for(@year, @month, @project, current_user)
     else
       date = Date.today
@@ -57,25 +57,6 @@ class WorkDaysController < ApplicationController
 
     def work_day_params
       params.require(:work_day).permit(:date, :start_time, :break, :end_time, :duration, :attendance, :notes, :user_id, :project_id)
-    end
-
-    def time_sheet_for(year, month, project)
-      if project.nil?
-        return nil
-      else
-        sheets = TimeSheet.where(year: year, month: month, project: project)
-        if sheets.empty?
-          return create_new_time_sheet(year, month, project)
-        else
-          return sheets.first
-        end
-      end
-    end
-
-    def create_new_time_sheet(year, month, project)
-      sheet = TimeSheet.create!({year: year, month: month, project_id: project.id, user_id: current_user.id, workload_is_per_month: true, salary_is_per_month: true})
-      sheet.save()
-      return sheet
     end
 
     def work_days_month_path
--- app/models/time_sheet.rb
+++ app/models/time_sheet.rb
@@ -4,4 +4,22 @@ class TimeSheet < ActiveRecord::Base
 
     validates :workload_is_per_month, :inclusion => { :in => [true, false] }
     validates :salary_is_per_month, :inclusion => { :in => [true, false] }
+
+    def self.time_sheet_for(year, month, project, user)
+      if project.nil?
+        return nil
+      else
+        sheets = TimeSheet.where(year: year, month: month, project: project, user: user)
+        if sheets.empty?
+          return create_new_time_sheet(year, month, project, user)
+        else
+          return sheets.first
+        end
+      end
+    end
+
+    def self.create_new_time_sheet(year, month, project, user)
+      sheet = TimeSheet.create!({year: year, month: month, project_id: project.id, user_id: user.id, workload_is_per_month: true, salary_is_per_month: true})
+      return sheet
+    end
 end
