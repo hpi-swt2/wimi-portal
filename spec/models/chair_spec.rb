635e782dbe8c1727fc6faf0fa60daf0238fa16a2
wgie <willi.gierke@futurice.com>
2015-12-12 20:58:01 +0100

    fixed chair annotations

--- spec/models/chair_spec.rb
+++ spec/models/chair_spec.rb
@@ -1,26 +1,13 @@
 # == Schema Information
 #
-# Table name: users
+# Table name: chairs
 #
-#  id                        :integer          not null, primary key
-#  email                     :string           default(""), not null
-#  sign_in_count             :integer          default(0), not null
-#  current_sign_in_at        :datetime
-#  last_sign_in_at           :datetime
-#  current_sign_in_ip        :string
-#  last_sign_in_ip           :string
-#  first_name                :string
-#  last_name                 :string
-#  created_at                :datetime         not null
-#  updated_at                :datetime         not null
-#  identity_url              :string
-#  residence                 :string
-#  street                    :string
-#  division_id               :integer          default(0)
-#  personnel_number          :integer          default(0)
-#  remaining_leave           :integer          default(28)
-#  remaining_leave_last_year :integer          default(0)
-#  superadmin                :boolean          default(FALSE)
+#  id           :integer          not null, primary key
+#  name         :string
+#  abbreviation :string
+#  description  :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
 #
 
 require 'rails_helper'
