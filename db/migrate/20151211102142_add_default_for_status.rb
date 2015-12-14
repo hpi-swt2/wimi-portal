d00207e007a3256f893e34479ab873bddc5b698a
Toni Stachewicz <toni.stachewicz@student.hpi.de>
2015-12-14 16:36:16 +0100

    added small fixes and tests

--- db/migrate/20151211102142_add_default_for_status.rb
+++ db/migrate/20151211102142_add_default_for_status.rb
@@ -1,7 +1,7 @@
 class AddDefaultForStatus < ActiveRecord::Migration
   def change
-    change_column :trips, :status, :integer, default: 1
-    change_column :holidays, :status, :integer, default: 1
-    add_column :expenses, :status, :integer, default: 1
+    change_column :trips, :status, :integer, default: 0
+    change_column :holidays, :status, :integer, default: 0
+    add_column :expenses, :status, :integer, default: 0
   end
 end
--- spec/controllers/requests_controller_spec.rb
+++ spec/controllers/requests_controller_spec.rb
@@ -5,11 +5,28 @@ RSpec.describe RequestsController, type: :controller do
   before(:each) do
     @user = FactoryGirl.create(:user)
     @chair = FactoryGirl.create(:chair)
-    sign_in @user
+    @representative = FactoryGirl.create(:user)
+    ChairWimi.create(user_id: @representative.id, chair_id: @chair.id, representative: true)
   end
 
-  it 'does not show requests for users' do
-    get :requests, { id: @chair}
-    expect(response).to have_http_status(302)
+  describe 'GET #requests' do
+    it 'does not show requests for users' do
+      sign_in @user
+      get :requests, {id: @chair}
+      expect(response).to have_http_status(302)
+    end
+
+    it 'shows request page for chairs representative' do
+      sign_in @representative
+      get :requests, {id: @chair}
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'does not show request page for another chair' do
+      chair1 = FactoryGirl.create(:chair)
+      sign_in @representative
+      get :requests, {id: chair1}
+      expect(response).to have_http_status(302)
+    end
   end
 end
