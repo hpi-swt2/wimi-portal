ab286f4c5105fb5d0401bfe9d74330fc0c7aedd6
Niklas Hoffmann <niklas.hoffmann@student.hpi.de>
2015-12-16 14:18:30 +0100

    added first tests for story

--- spec/views/dashboard/index.html.erb_spec.rb
+++ spec/views/dashboard/index.html.erb_spec.rb
@@ -23,4 +23,44 @@ RSpec.describe 'dashboard/index.html.erb', type: :view do
     expect(rendered).to have_content('You have been invited to a project')
     expect(Notification.where(user: @user).size).to eq 0
   end
+
+  it 'shows content for users without any chair or project' do
+    login_as(@user, :scope => :user)
+    chair1 = FactoryGirl.create(:chair, name: 'Chair1')
+    chair2 = FactoryGirl.create(:chair, name: 'Chair2')
+    visit dashboard_path
+
+    expect(page).to have_content(chair1.name)
+    expect(page).to have_content(chair2.name)
+    expect(page).to have_content('Apply')
+  end
+
+  it 'performs an application after click on Apply' do
+    login_as(@user, :scope => :user)
+    chair1 = FactoryGirl.create(:chair, name: 'Chair1')
+    visit dashboard_path
+
+    expect(page).to have_content(chair1.name)
+    expect(page).to have_content('Apply')
+    expect(page).to_not have_content('pending')
+    click_on 'Apply'
+    expect(page).to have_content('pending')
+    expect(page).to_not have_content('Apply')
+  end
+
+  it 'shows invitations for projects' do
+  #   TODO
+  end
+
+  it 'hides the content for chairless and projectless users for all other users' do
+    chair1 = FactoryGirl.create(:chair, name: 'Chair1')
+    chair2 = FactoryGirl.create(:chair, name: 'Chair2')
+    chairwimi = ChairWimi.create(user_id: @user.id, chair_id: chair1.id)
+    login_as(@user, :scope => :user)
+    visit dashboard_path
+
+    expect(page).to_not have_content(chair1.name)
+    expect(page).to_not have_content(chair2.name)
+    expect(page).to_not have_content('Apply')
+  end
 end
