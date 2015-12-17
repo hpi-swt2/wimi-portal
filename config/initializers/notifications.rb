04a33c5f700b8407183d8efb9eb50ea68ba91b7d
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2015-12-17 17:36:16 +0100

    add Event documentation

--- config/initializers/notifications.rb
+++ config/initializers/notifications.rb
@@ -1,3 +1,22 @@
+# Event Template:
+#
+# ActiveSupport::Notifications.subscribe "<event_name>" do |name,start,finish,id,payload|
+# --handle your event here--
+# --for example make a new model (MyEvent)  and save the payload within:
+#   MyEvent.create! do |event|
+#     event.attribute = payload[:key]
+#   end
+# end
+# 
+# To trigger the event call the instrument function where your event should be created (for example in the controller) with
+#
+# ActiveSupport::Notifications.instrument("<event_name>", { :key => value })
+#
+# where { :key => value } can be any custom hashmap you like.
+# this map will be available through the payload variable in the subscribe function
+#
+# add new events here
+
 ActiveSupport::Notifications.subscribe "event.admin.rights_changed" do |name,start,finish,id,payload|
   EventAdminRightsChanged.create! do |event|
     event.admin = payload[:admin]
