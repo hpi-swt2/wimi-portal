7f16eee1f1ff82c93215aac39a622484dedfa456
willi.gierke <willi.gierke@student.hpi.uni-potsdam.de>
2015-12-19 12:12:34 +0100

    removed trailing whitespaces and added annotations

--- spec/models/travel_expense_report_item_spec.rb
+++ spec/models/travel_expense_report_item_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: travel_expense_report_items
+#
+#  id                       :integer          not null, primary key
+#  date                     :date
+#  breakfast                :boolean
+#  lunch                    :boolean
+#  dinner                   :boolean
+#  travel_expense_report_id :integer
+#  created_at               :datetime         not null
+#  updated_at               :datetime         not null
+#  annotation               :text
+#
+
 require 'rails_helper'
 
 RSpec.describe TravelExpenseReportItem, type: :model do
--- spec/models/travel_expense_report_spec.rb
+++ spec/models/travel_expense_report_spec.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: travel_expense_reports
+#
+#  id               :integer          not null, primary key
+#  inland           :boolean
+#  country          :string
+#  location_from    :string
+#  location_via     :string
+#  location_to      :string
+#  reason           :text
+#  date_start       :datetime
+#  date_end         :datetime
+#  car              :boolean
+#  public_transport :boolean
+#  vehicle_advance  :boolean
+#  hotel            :boolean
+#  general_advance  :integer
+#  user_id          :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  signature        :boolean
+#
+
 require 'rails_helper'
 
 RSpec.describe TravelExpenseReport, type: :model do
