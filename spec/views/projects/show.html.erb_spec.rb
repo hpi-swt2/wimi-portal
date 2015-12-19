474a3f6b2039345ed0b8ff997683f13233019929
Theresa <theresa.zobel@student.hpi.de>
2015-12-19 12:54:02 +0100

    trying to fix tests

--- spec/views/projects/show.html.erb_spec.rb
+++ spec/views/projects/show.html.erb_spec.rb
@@ -3,16 +3,17 @@ require 'rails_helper'
 RSpec.describe 'projects/show', type: :view do
   before(:each) do
 
-    @wimi = FactoryGirl.create(:chair_representative, first_name:'Theresa', last_name:'Zobel', superadmin: true)
-    @user = FactoryGirl.create(:user)
+    # @wimi = FactoryGirl.create(:chair_representative, first_name:'Theresa', last_name:'Zobel', superadmin: true)
+    # @user = FactoryGirl.create(:user)
     # @project = assign(:project, Project.create!(:title => 'My Project'))
   end
 
 
   it 'has information about the project on page as a Wimi' do
+    @wimi = FactoryGirl.create(:chair_representative, first_name:'Theresa', last_name:'Zobel', superadmin: true)
     login_as @wimi
     @project = FactoryGirl.create(:project, title:'BP1', chair: @wimi.chair, project_leader:'')
-    byebug
+    # byebug
     visit "/projects/#{@project.id}"
     # print page.html
 
