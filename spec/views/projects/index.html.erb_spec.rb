cd6c791c7994f830cf4c1c53eefb08808840ee80
Theresa <theresa.zobel@student.hpi.de>
2015-12-20 13:42:54 +0100

    refactoring

--- spec/views/projects/index.html.erb_spec.rb
+++ spec/views/projects/index.html.erb_spec.rb
@@ -7,25 +7,4 @@ RSpec.describe 'projects/index', type: :view do
 
   end
 
-  # it 'changes checkbox id after user was assigned to a project' do
-  #   project1 = FactoryGirl.create(:project)
-  #   visit "/projects/#{project1.id}/edit"
-  #   check "project_user_ids_#{@user.id}"
-  #   click_button('Update Project')
-  #   visit "/projects/#{project1.id}/edit"
-  #   page.find('#currentUserCheckbox')
-  # end
-
-  # it 'has the accessibility public after a project was newly created' do
-  #   project2 = FactoryGirl.create(:project)
-  #   visit "/projects/#{project2.id}/edit"
-  #   expect(page).to have_selector 'input[type=radio][checked=checked][id=project_public_true]'
-  # end
-  #
-  # it 'has the status active after a project was newly created' do
-  #   project3 = FactoryGirl.create(:project)
-  #   visit "/projects/#{project3.id}/edit"
-  #   expect(page).to have_selector 'input[type=radio][checked=checked][id=project_status_true]'
-  # end
-
 end
