a17fbae26406ac5b10103d4b2ebeaa20bdec1b13
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2016-01-07 15:42:45 +0100

    add missing localization, change hashset format

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151215145322) do
+ActiveRecord::Schema.define(version: 20151215185150) do
 
   create_table "chair_wimis", force: :cascade do |t|
     t.boolean "admin",          default: false
@@ -192,7 +192,6 @@ ActiveRecord::Schema.define(version: 20151215145322) do
     t.string   "language",                  default: "en",  null: false
     t.string   "residence"
     t.string   "street"
-    t.integer  "division_id",               default: 0
     t.integer  "personnel_number",          default: 0
     t.integer  "remaining_leave",           default: 28
     t.integer  "remaining_leave_last_year", default: 0
