c8cc262f870fba51976b6449fffb4cdda95a1215
willi.gierke <willi.gierke@student.hpi.uni-potsdam.de>
2016-01-07 12:04:41 +0100

    added annotations

--- app/models/time_sheet.rb
+++ app/models/time_sheet.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: time_sheets
+#
+#  id                    :integer          not null, primary key
+#  month                 :integer
+#  year                  :integer
+#  salary                :integer
+#  salary_is_per_month   :boolean
+#  workload              :integer
+#  workload_is_per_month :boolean
+#  user_id               :integer
+#  project_id            :integer
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#
+
 class TimeSheet < ActiveRecord::Base
     belongs_to :user
     belongs_to :project
