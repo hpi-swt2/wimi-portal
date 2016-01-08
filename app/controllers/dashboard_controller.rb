be26af1c1c088099a7cc83202280848ad65a8d64
sebastian-ernst <s.ernst94@me.com>
2016-01-08 16:27:16 +0100

    fg-dashboard applied status update

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,10 @@
 class DashboardController < ApplicationController
   def index
     @invitations = Invitation.where(user: current_user)
-    @applied_requests = collectAplliedRequests
+    @applied_requests = collectAppliedRequests
   end
 
-  def collectAplliedRequests
+  def collectAppliedRequests
     requests = Array.new
 
     current_user.chair.users.each do |user|
@@ -13,13 +13,13 @@ class DashboardController < ApplicationController
         requests << {type: "holiday", handed_in: holiday.created_at, request: holiday}
       end
 
-      # applied_expense = user.expenses.select { |e| e.status == 'applied' }
-      user.expenses.each do |expense|
+      applied_expenses = user.expenses.select { |e| e.status == 'applied' }
+      applied_expenses.each do |expense|
         requests << {type: "expense", handed_in: expense.created_at, request: expense}
       end
 
-      # applied_trip = user.trips.select { |t| t.status == 'applied' }
-      user.trips.each do |trip|
+      applied_trips = user.trips.select { |t| t.status == 'applied' }
+      applied_trips.each do |trip|
         requests << {type: "trip", handed_in: trip.created_at, request: trip}
       end
     end
