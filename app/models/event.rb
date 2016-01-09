e1f1b90ef12b9a44f80223a5d2115840d22bec4d
Jan van Dieken <Zanthelo@users.noreply.github.com>
2016-01-09 14:36:54 +0100

    copies event implementation from feature/events

---
+++ app/models/event.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: events
+#
+# id          :integer      not null, primary key
+# trigger     :integer      foreign key
+# target      :integer      foreign key
+# chair       :integer      foreign key
+# seclevel    :integer     
+# type        :string
+class Event < ActiveRecord::Base
+  belongs_to :chair
+  belongs_to :trigger, class_name: 'User'
+
+  validates :type, presence: true
+  validates :chair, presence: true
+  validates :seclevel, presence: true
+
+  enum seclevel: [ :superadmin, :admin, :representative, :wimi, :hiwi, :user]
+
+end
