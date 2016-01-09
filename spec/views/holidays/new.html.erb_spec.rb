e6ecfadb5fb90e7668632ab7c8636e6da16dafb5
tymolc <tim.oesterreich@student.hpi.de>
2016-01-09 23:23:09 +0100

    Merge remote-tracking branch 'origin/dev' into feature/55_holidays_details

--- spec/views/holidays/new.html.erb_spec.rb
+++ spec/views/holidays/new.html.erb_spec.rb
@@ -16,15 +16,16 @@ RSpec.describe 'holidays/new', type: :view do
   	chair = FactoryGirl.create(:chair)
   	chair2 = FactoryGirl.create(:chair)
     user1 = FactoryGirl.create(:user, division_id: 1)
-  	user2 = FactoryGirl.create(:user, division_id: 1)
-  	user3 = FactoryGirl.create(:user, division_id: 1)
-  	user4 = FactoryGirl.create(:user, division_id: 2)
+  	user2 = FactoryGirl.create(:user, division_id: 1, first_name: 'name1')
+  	user3 = FactoryGirl.create(:user, division_id: 1, first_name: 'name2')
+  	user4 = FactoryGirl.create(:user, division_id: 2, first_name: 'name3')
   	chair1wimi = FactoryGirl.create(:chair_wimi, user_id: user2.id, chair_id: chair.id, admin: true)
   	chair2wimi = FactoryGirl.create(:chair_wimi, user_id: user4.id, chair_id: chair2.id, admin: true)
-  	sign_in user1
+  	login_as user1
 
-  	render
-  	find(:html, "checkbox").set(true)
-  	assert_select 'div>div', text: 'John Doe'.to_s, count: 1
+  	visit new_holiday_path
+  	check('checkbox')
+  	replacement_select = page.find(:xpath, '//html//body//div//div//form//div[5]//div//select')
+  	expect(replacement_select).to have_option(1)
   end
 end
