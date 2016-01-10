ee320b31ec3ab4593e8e344d34e07793fb5635de
Jonas Bounama <jonas.bounama@student.hpi.uni-potsdam.de>
2016-01-10 12:07:24 +0100

    removed unnecessary comments

--- spec/views/work_days/index.html.erb_spec.rb
+++ spec/views/work_days/index.html.erb_spec.rb
@@ -16,8 +16,6 @@ RSpec.describe "work_days/index.html.erb", type: :view do
 		user_id: @superadmin.id, project_id: @project.id, handed_in: false)
 	    visit work_days_path(month: Date.today.month, year: Date.today.year, project: @project.id, user_id: @superadmin.id)
 	    page.should have_selector("input[type=submit][value='sign']")
-	    #page.find('.sign', visible: false)
-    	#expect(page).to have_content('sign')
 	end
 
 	it 'expects sign-button for handed in timesheets' do
@@ -26,8 +24,6 @@ RSpec.describe "work_days/index.html.erb", type: :view do
 		user_id: @superadmin.id, project_id: @project.id, handed_in: true, last_modified: Date.today)
 		visit work_days_path(month: Date.today.month, year: Date.today.year, project: @project.id, user_id: @superadmin.id)
 		page.should have_selector("input[type=submit][value='sign']")
-		#page.find('.sign', visible: false)
-		#expect(page).to have_content('sign')
 	end
 
 	it 'expects reject-button for handed in timesheets' do
@@ -36,8 +32,6 @@ RSpec.describe "work_days/index.html.erb", type: :view do
 		user_id: @superadmin.id, project_id: @project.id, handed_in: true, last_modified: Date.today)
 		visit work_days_path(month: Date.today.month, year: Date.today.year, project: @project.id, user_id: @superadmin.id)
 		page.should have_selector("input[type=submit][value='reject']")
-		#page.find('.reject', visible: false)
-		#expect(page).to have_content('reject')	
 	end
 
 end
