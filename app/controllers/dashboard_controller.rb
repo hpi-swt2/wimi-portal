3e41cc5d9cc61738226d9e5e8c1cbf5b7a15ef3e
Jan van Dieken <Zanthelo@users.noreply.github.com>
2016-01-10 15:43:51 +0100

    fixes dashboard view test

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,5 @@
 class DashboardController < ApplicationController
   def index
-    @invitations = Invitation.where(user: current_user)
     @notifications = Event.select{|event| event.target == current_user}
   end
 end
