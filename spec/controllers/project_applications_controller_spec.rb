80933c2a4276360ff6e2af4e850be4fa19029af7
Arne Boockmeyer <github@arneboockmeyer.de>
2016-01-11 19:00:59 +0100

    use double point notation instead of arrows

--- spec/controllers/project_applications_controller_spec.rb
+++ spec/controllers/project_applications_controller_spec.rb
@@ -56,25 +56,25 @@ RSpec.describe ProjectApplicationsController, type: :controller do
     context "with valid params" do
       it "creates a new ProjectApplication" do
         expect {
-          post :create, {:project_application => valid_attributes, id: @project.id}, valid_session
+          post :create, {project_application: valid_attributes, id: @project.id}, valid_session
         }.to change(ProjectApplication, :count).by(1)
       end
 
       it "assigns a newly created project_application as @project_application" do
-        post :create, {:project_application => valid_attributes, id: @project.id}, valid_session
+        post :create, {project_application: valid_attributes, id: @project.id}, valid_session
         expect(assigns(:project_application)).to be_a(ProjectApplication)
         expect(assigns(:project_application)).to be_persisted
       end
 
       it "redirects to the project_application index" do
-        post :create, {:project_application => valid_attributes, id: @project.id}, valid_session
+        post :create, {project_application: valid_attributes, id: @project.id}, valid_session
         expect(response).to redirect_to(ProjectApplication)
       end
     end
 
     context "with invalid params" do
       it "redirects back to index page" do
-        post :create, {:project_application => invalid_attributes, id: @project.id}, valid_session
+        post :create, {project_application: invalid_attributes, id: @project.id}, valid_session
         expect(response).to redirect_to(project_applications_path)
       end
     end
@@ -85,13 +85,13 @@ RSpec.describe ProjectApplicationsController, type: :controller do
     it "destroys the requested project_application" do
       project_application = ProjectApplication.create! valid_attributes
       expect {
-        delete :destroy, {:id => project_application.to_param}, valid_session
+        delete :destroy, {id: project_application.to_param}, valid_session
       }.to change(ProjectApplication, :count).by(-1)
     end
 
     it "redirects to the project_applications list" do
       project_application = ProjectApplication.create! valid_attributes
-      delete :destroy, {:id => project_application.to_param}, valid_session
+      delete :destroy, {id: project_application.to_param}, valid_session
       expect(response).to redirect_to(:back)
     end
   end
@@ -100,7 +100,7 @@ RSpec.describe ProjectApplicationsController, type: :controller do
     it "accepts the project application" do
       project_application = ProjectApplication.create! valid_attributes
       login_as @wimi
-      get :accept, {:id => project_application.id}, valid_session
+      get :accept, {id: project_application.id}, valid_session
       expect(response).to redirect_to(:back)
       expect(project_application.reload.accepted?).to eq(true)
     end
@@ -110,7 +110,7 @@ RSpec.describe ProjectApplicationsController, type: :controller do
     it "declines the project application" do
       project_application = ProjectApplication.create! valid_attributes
       login_as @wimi
-      get :decline, {:id => project_application.id}, valid_session
+      get :decline, {id: project_application.id}, valid_session
       expect(response).to redirect_to(:back)
       expect(project_application.reload.declined?).to eq(true)
     end
@@ -120,7 +120,7 @@ RSpec.describe ProjectApplicationsController, type: :controller do
     it "reapplies for a project" do
       project_application = ProjectApplication.create! valid_attributes
       project_application.update(status: :declined)
-      get :reapply, {:id => project_application.id}, valid_session
+      get :reapply, {id: project_application.id}, valid_session
       expect(response).to redirect_to(:back)
       expect(project_application.reload.pending?).to eq(true)
     end
