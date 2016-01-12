37e4a8a828dd071562e7c5a4931698294cc2edb7
Jan van Dieken <Zanthelo@users.noreply.github.com>
2016-01-12 21:20:53 +0100

    refactors eventselection

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
   def index
-    @notifications = Event.select{|event| event.target == current_user}
+    @notifications = Event.select{|event| event.target == current_user && event.type == "EventProjectInvitation"}.reverse
+    @notifications.concat(Event.select{|event| event.target == current_user && (event.type == "EventTimeSheetAccepted" || event.type == "EventTimeSheetDeclined")}.reverse)
     current_user.projects.each do |project|
       @notifications.concat(Event.select{|event| (event.target == project) && (event[:seclevel] >= Event.seclevel_of_user(current_user))})
     end
