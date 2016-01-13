b67bde52c9cb151d09bac749b6099f0b28c259ce
Jonas Bounama <jonas.bounama@student.hpi.uni-potsdam.de>
2016-01-13 15:54:48 +0100

    removed routes comments

--- config/routes.rb
+++ config/routes.rb
@@ -19,8 +19,6 @@ Rails.application.routes.draw do
   get 'dashboard', to: 'dashboard#index'
   get 'users/edit_leave', to: 'users#edit_leave'
 
-    # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
   resources :publications
   resources :projects do
     member do
