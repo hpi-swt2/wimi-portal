2977611996c011a630172c2cc8d59a27c3a36eae
willi.gierke <willi.gierke@student.hpi.uni-potsdam.de>
2016-01-14 01:31:58 +0100

    added login test

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -30,3 +30,57 @@ describe 'login via OpenID' do
     end
   end
 end
+
+describe 'login via email/password' do
+  before :each do
+    visit '/'
+    expect(page).to have_content 'Please login first'
+  end
+
+  it 'should login the superadmin with his credentials' do
+    superadmin = FactoryGirl.create(:user, superadmin: true, email: 'wimi-admin', password: 'wimi-admin-password')
+    visit '/superadmin'
+
+    fill_in 'user_email', with: superadmin.email
+    fill_in 'user_password', with: superadmin.password
+    click_on 'Log in'
+
+    expect(page).to_not have_content 'Please login first'
+    routes = ['/publications', '/projects', '/holidays', '/trips', '/expenses']
+    routes.each do |route|
+      visit route
+      expect(page).to have_content 'Logout'
+    end
+  end
+
+  it 'should not login the superadmin with wrong credentials' do
+    superadmin = FactoryGirl.create(:user, superadmin: true, email: 'wimi-admin', password: 'wimi-admin-password')
+    visit '/superadmin'
+
+    fill_in 'user_email', with: superadmin.email
+    fill_in 'user_password', with: 'wrong_password'
+    click_on 'Log in'
+
+    routes = ['/publications', '/projects', '/holidays', '/trips', '/expenses']
+    routes.each do |route|
+      visit route
+      expect(page).to_not have_content 'Logout'
+    end
+  end
+
+  it 'should not login a user' do
+    user = FactoryGirl.create(:user, email: 'no-admin')
+    visit '/superadmin'
+
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_on 'Log in'
+
+    expect(page).to have_content 'Please sign in'
+    routes = ['/publications', '/projects', '/holidays', '/trips', '/expenses']
+    routes.each do |route|
+      visit route
+      expect(page).to_not have_content 'Logout'
+    end
+  end
+end
