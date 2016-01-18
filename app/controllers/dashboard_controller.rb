70c8d23f22cc0aeb375112b4ca808a34a541072f
Toni Stachewicz <toni.stachewicz@student.hpi.de>
2016-01-18 09:48:07 +0100

    notifications for other chair members invisible

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -10,6 +10,7 @@ class DashboardController < ApplicationController
 
     @notifications += Event.where(seclevel: Event.seclevels[:user])
 
+    @notifications.delete_if { |event| event.chair_id != current_user.chair.id }
     @notifications.delete_if { |event| event.is_hidden_by(current_user) }
     @notifications = @notifications.sort_by { |n| n[:created_at] }.reverse
 
