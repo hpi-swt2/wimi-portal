0488609cd472202db34bb4cdbcb0b2444511f309
Jan van Dieken <Zanthelo@users.noreply.github.com>
2016-01-20 12:25:58 +0100

    fixes which notifications should be displayed

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,25 +1,31 @@
 class DashboardController < ApplicationController
   def index
     @notifications = Array.new
+    @temp = Array.new
+    seclevel = Event.seclevel_of_user(current_user)
 
-    @notifications += Event.select{|event| event.target_id == current_user.id && event.type == "EventProjectInvitation"}
-    @notifications += Event.select{|event| event.target_id == current_user.id && (event.type == "EventTimeSheetAccepted" || event.type == "EventTimeSheetDeclined")}
+    @temp += Event.where(seclevel: Event.seclevels[:superadmin]) if current_user.is_superadmin?
+    @temp += Event.where(seclevel: Event.seclevels[:admin]) if current_user.is_admin?
+    @temp += Event.where(seclevel: Event.seclevels[:representative]) if current_user.is_representative?
+    @temp += Event.where(seclevel: Event.seclevels[:wimi]) if current_user.is_wimi?
+    @temp += Event.where(seclevel: Event.seclevels[:hiwi]) if current_user.is_hiwi?
+    @temp += Event.where(seclevel: Event.seclevels[:user])
+
+    @notifications += Event.select{|event| event.target_id == current_user.id && event.type == 'EventProjectInvitation'}
+    @notifications += @temp.select{|event| event.target_id == current_user.id && (event.type == 'EventTimeSheetAccepted' || event.type == "EventTimeSheetDeclined")}
     current_user.projects.each do |project|
-      @notifications += Event.select{|event| (event.target == project) && (event[:seclevel] >= Event.seclevel_of_user(current_user))}
-      @notifications += Event.select{|event| event.target == project && event[:seclevel] == Event.seclevel_of_user(current_user) && event.type == "EventTimeSheetSubmitted"}
+      @notifications += @temp.select{|event| event.target == project}
+      @notifications += @temp.select{|event| event.target == project && event.type == "EventTimeSheetSubmitted"}
     end
 
-    @notifications += Event.where(seclevel: Event.seclevels[:superadmin]) if current_user.is_superadmin?
-    @notifications += Event.where(seclevel: Event.seclevels[:admin]) if current_user.is_admin?
-    @notifications += Event.where(seclevel: Event.seclevels[:representative]) if current_user.is_representative?
-    @notifications += Event.where(seclevel: Event.seclevels[:wimi]) if current_user.is_wimi?
-    @notifications += Event.where(seclevel: Event.seclevels[:hiwi]) if current_user.is_hiwi?
-
-    @notifications += Event.where(seclevel: Event.seclevels[:user])
+    @notifications += @temp.select{|event| event.target_id == current_user.id && event.type == 'EventAdminRight'}
+    @notifications += @temp.select{|event| event.chair == current_user.chair && event.type == 'EventChairApplication'}
+    @notifications += @temp.select{|event| event.chair == current_user.chair && (event.status == 'holiday' || event.status == 'travel_expense_report' || event.status == 'trip') && event.type == 'EventRequest'}
+    @notifications += @temp.select{|event| event.chair == current_user.chair && event.type == 'EventUserChair'}
 
     #@notifications.delete_if { |event| event.chair_id != current_user.chair.id }
     #@notifications.delete_if { |event| event.is_hidden_by(current_user) }
-    #@notifications = @notifications.sort_by { |n| n[:created_at] }.reverse
+    @notifications = @notifications.sort_by { |n| n[:created_at] }.reverse
 
     #@invitations = Invitation.where(user: current_user)
   end
