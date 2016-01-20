288eec7e2fc733432a4b68f3d990b59759cce487
Tim Oesterreich <tim.oesterreich1@gmail.com>
2016-01-20 13:30:59 +0100

    fixed merge failures

--- app/assets/javascripts/bootstrap.js.coffee
+++ app/assets/javascripts/bootstrap.js.coffee
@@ -2,4 +2,3 @@ jQuery ->
   $("a[rel~=popover], .has-popover").popover()
   $("a[rel~=tooltip], .has-tooltip").tooltip()
   $('.datepicker').datepicker()
-  
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -31,6 +31,9 @@ class Ability
     cannot :create, ProjectApplication do |project_application|
       user.projects.exists?(project_application.project_id)
     end
+    can :leave_project, Project do |project|    
+      project.users.include? user   
+    end
     # can :accept_invitation, Project
     # can :manage, Stundenzettel
   end
@@ -44,6 +47,9 @@ class Ability
     end
     can :invite_user, Project do |project|
       project.users.include? user
+    end
+    can :leave_project, Project do |project|    
+      project.users.include? user   
     end
     can :manage, ProjectApplication do |project_application|
       user.projects.exists?(project_application.project_id)
