97e78537d755c43d3737e0fe9536f6e72f525543
wgie <willi.gierke@futurice.com>
2016-01-21 23:02:50 +0100

    added linebreaks

--- spec/factories/hiwi.rb
+++ spec/factories/hiwi.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :hiwi, class: 'User' do
     first_name          'John'
     last_name           'Doe'
-    sequence(:email)    { |_n| 'person#{n}@example.com' }
+    sequence(:email)    { |n| "person#{n}@example.com" }
     projects            {build_list :project, 1}
   end
 end
