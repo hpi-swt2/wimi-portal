067ef5802223cadeda44e1455a7032e060bee1db
Jan van Dieken <Zanthelo@users.noreply.github.com>
2016-01-24 22:15:13 +0100

    removes seclevel_of_user method

--- app/models/event.rb
+++ app/models/event.rb
@@ -29,20 +29,4 @@ class Event < ActiveRecord::Base
   def hide_for(user)
     UserEvent.create(user: user, event: self) unless is_hidden_by(user)
   end
-
-  def self.seclevel_of_user(user)
-    if user.superadmin
-      return Event.seclevels[:superadmin]
-    elsif user.is_admin?
-      return Event.seclevels[:admin]
-    elsif user.is_representative?
-      return Event.seclevels[:representative]
-    elsif user.is_wimi?
-      return Event.seclevels[:wimi]
-    elsif user.is_hiwi?
-      return Event.seclevels[:hiwi]
-    else
-      return Event.seclevels[:user]
-    end
-  end
 end
