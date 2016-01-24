44c7ef986ecbeef8068fe8534cb57c76583cf23b
Jan van Dieken <Zanthelo@users.noreply.github.com>
2016-01-24 22:50:39 +0100

    swaps names of notifications and activities

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   def index
-    @notifications = []
     @activities = []
+    @notifications = []
     temp = []
 
     temp += Event.where(seclevel: Event.seclevels[:superadmin]) if current_user.is_superadmin?
@@ -11,27 +11,28 @@ class DashboardController < ApplicationController
     temp += Event.where(seclevel: Event.seclevels[:hiwi]) if current_user.is_hiwi?
     temp += Event.where(seclevel: Event.seclevels[:user])
 
-    @activities += Event.select{|event| event.target_id == current_user.id && event.type == 'EventProjectInvitation'}
-    @activities += temp.select{|event| event.chair == current_user.chair && event.type == 'EventChairApplication'}
-    @notifications += temp.select{|event| event.target_id == current_user.id && (event.type == 'EventTimeSheetAccepted' || event.type == "EventTimeSheetDeclined")}
-    current_user.projects.each do |project|
-      @notifications += temp.select{|event| event.target == project && event.type == "EventTimeSheetSubmitted"}
-    end
+    @notifications += Event.select{|event| event.target_id == current_user.id && event.type == 'EventProjectInvitation'}
+    @notifications += temp.select{|event| event.chair == current_user.chair && event.type == 'EventChairApplication'}
 
-    @notifications += temp.select{|event| event.target_id == current_user.id && event.type == 'EventAdminRight'}
-    @notifications += temp.select{|event| event.chair == current_user.chair && (event.status == 'holiday' || event.status == 'travel_expense_report' || event.status == 'trip') && event.type == 'EventRequest'}
-    @notifications += temp.select{|event| event.chair == current_user.chair && event.type == 'EventUserChair'}
-    @notifications += temp.select{|event| event.target_id == current_user.id && (event.type == 'EventTravelRequestAccepted' || event.type == 'EventTravelRequestDeclined')}
-
-    #@notifications.delete_if { |event| event.chair_id != current_user.chair.id }
-    #@notifications.delete_if { |event| event.is_hidden_by(current_user) }
-    @activities = @activities.sort_by { |n| n[:created_at] }.reverse
     @notifications = @notifications.sort_by { |n| n[:created_at] }.reverse
-    to_delete = @notifications.drop(50)
+
+    @activities += temp.select{|event| event.target_id == current_user.id && (event.type == 'EventTimeSheetAccepted' || event.type == "EventTimeSheetDeclined")}
+    current_user.projects.each do |project|
+      @activities += temp.select{|event| event.target == project && event.type == "EventTimeSheetSubmitted"}
+    end
+    @activities += temp.select{|event| event.target_id == current_user.id && event.type == 'EventAdminRight'}
+    @activities += temp.select{|event| event.chair == current_user.chair && (event.status == 'holiday' || event.status == 'travel_expense_report' || event.status == 'trip') && event.type == 'EventRequest'}
+    @activities += temp.select{|event| event.chair == current_user.chair && event.type == 'EventUserChair'}
+    @activities += temp.select{|event| event.target_id == current_user.id && (event.type == 'EventTravelRequestAccepted' || event.type == 'EventTravelRequestDeclined')}
+
+    @activities.delete_if { |event| event.is_hidden_by(current_user) }
+    @activities = @activities.sort_by { |n| n[:created_at] }.reverse
+
+    to_delete = @activities.drop(50)
     unless to_delete.empty?
       to_delete.delete_all
     end
 
-    #@invitations = Invitation.where(user: current_user)
+    @activities = @activities.take(50)
   end
 end
