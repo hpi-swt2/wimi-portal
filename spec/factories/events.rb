0a2158db4ab6867b34dce28ae2d3300325d1283e
Jan van Dieken <Zanthelo@users.noreply.github.com>
2016-01-25 15:58:02 +0100

    removes unnecessary comment

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -21,12 +21,3 @@ FactoryGirl.define do
     type "Event"
   end
 end
-
-#FactoryGirl.define do
-#  factory :event, class: 'Event' do
-#    trigger_id 1
-#    seclevel :admin
-#    type 'EventUserChair'
-#    chair {FactoryGirl.create(:chair)}
-#    end
-#end
