546d1e2e7b66fd4e131def5066be6563b83dc2b3
Jan van Dieken <Zanthelo@users.noreply.github.com>
2016-01-25 18:30:10 +0100

    adds test for automatic deletion of events

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -17,4 +17,28 @@ RSpec.describe DashboardController, type: :controller do
     get :index
     expect(response).to render_template('index')
   end
+
+  it 'deletes every activity that is older than the last 50' do
+    chair = FactoryGirl.create(:chair)
+    project = FactoryGirl.create(:project, chair: chair)
+    user = FactoryGirl.create(:hiwi)
+    time_sheet = FactoryGirl.create(:time_sheet, user_id: user.id, project_id: project.id)
+    60.times do |i|
+      ActiveSupport::Notifications.instrument('event', {trigger: time_sheet.id, target: project.id, seclevel: :wimi, type: 'EventTimeSheetSubmitted'})
+    end
+
+    expect(Event.all.size).to eq(60)
+
+    wimi = User.find(FactoryGirl.create(:wimi, chair: chair).user_id)
+    wimi.projects = []
+    wimi.projects << project
+    project.users << wimi
+
+    expect(wimi.is_wimi?).to eq(true)
+
+    login_with(wimi)
+    get :index
+
+    expect(Event.all.size).to eq(50)
+  end
 end
