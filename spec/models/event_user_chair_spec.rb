ac303c236558e06ea093b6715d5e3f9352a85b29
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2016-01-29 15:44:21 +0100

    pray to the travis god

--- spec/models/event_user_chair_spec.rb
+++ spec/models/event_user_chair_spec.rb
@@ -18,9 +18,9 @@ require 'rails_helper'
 RSpec.describe EventUserChair, type: :model do
   describe 'GET #index' do
     it 'sets defaults for seclevel and type' do
-      chair = FactoryGirl.build_stubbed(:chair)
-      trigger = FactoryGirl.build_stubbed(:user)
-      target = FactoryGirl.build_stubbed(:user)
+      chair = FactoryGirl.create(:chair)
+      trigger = FactoryGirl.create(:user)
+      target = FactoryGirl.create(:user)
       @event = EventUserChair.create(trigger_id: trigger.id, target_id: target.id, chair_id: chair.id)
 
       expect(@event.seclevel).to eq('admin')
