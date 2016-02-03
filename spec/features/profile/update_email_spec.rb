6549741daa7e694ad79151fb1f4e341d301e7852
Nico Aechtner <nico.aechtner@student.hpi.de>
2016-02-03 13:22:21 +0100

    add validation for email for user profile change

---
+++ spec/features/profile/update_email_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'updating email address' do
+  before :each do
+    @user = FactoryGirl.create(:user)
+    login_as @user
+    visit edit_user_path(@user.id)
+  end
+
+  it 'should update with a valid email address' do
+    valid_email = 'valid@e.mail'
+    fill_in :user_email, with: valid_email
+    click_button 'Save'
+    expect(@user.reload.email).to eq(valid_email)
+  end
+
+  it 'should not update without a valid email address' do
+    invalid_email = 'invalid mail'
+    former_mail = @user.email
+    fill_in :user_email, with: invalid_email
+    click_button 'Save'
+    expect(page).to have_text('is not a valid email address.')
+    expect(@user.reload.email).to eq(former_mail)
+  end
+end
