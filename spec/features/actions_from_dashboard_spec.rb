4d2bb0f18c2b090283219c617d9ba53d1e7378e9
Christoph Matthies <christoph.matthies@gmail.com>
2016-06-07 14:18:32 +0200

    Add acceptance test of dashboard links.

---
+++ spec/features/actions_from_dashboard_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'actions from dashboard' do
+  before :each do
+    @user = FactoryGirl.create(:user)
+    @project = FactoryGirl.create(:project)
+    FactoryGirl.create(:contract, hiwi: @user)
+    @project.users << @user
+    login_as @user
+    visit dashboard_path
+  end
+
+  it 'should be possible to add working hours for a project from the dashboard' do
+    click_link(I18n.t('dashboard.actions.enter_work_hours', project_name: @project.title))
+    expect(current_path).to eq(new_work_day_path)
+    expect(page).to have_content(@project.title)
+  end
+
+end
