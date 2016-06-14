a4246bd0fdee997bafe47f8defcb272ed02bbf42
Christoph Matthies <christoph.matthies@gmail.com>
2016-06-14 15:01:03 +0200

    Disable verbose server output in development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,8 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  config.log_level = :warn
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
