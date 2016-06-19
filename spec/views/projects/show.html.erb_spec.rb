17fb6a18d482d9fa477faaf081862a75c969ac2d
Christoph Matthies <christoph.matthies@gmail.com>
2016-06-19 16:01:22 +0200

    Refactor viesw/projects/show spec.

--- spec/views/projects/show.html.erb_spec.rb
+++ spec/views/projects/show.html.erb_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe 'projects/show', type: :view do
 
     expect(page).to have_content(project.title)
     expect(page).to have_content(project.chair.name)
-    expect(page).to_not have_content('public')
+    expect(page).to_not have_content(I18n.t('projects.form.show.public'))
     
     # only because rep is also wimi of the project
     expect(page).to have_content(project.chair.representative.user.name)
@@ -42,29 +42,24 @@ RSpec.describe 'projects/show', type: :view do
     it 'has information about the project on page' do
       expect(page).to have_content(@project.title)
       expect(page).to have_content(@project.chair.name)
-      expect(page).to_not have_content('public')
+      expect(page).to_not have_content(I18n.t('projects.form.show.public'))
     end
 
-    # hiwi can't see project if not a member
-#    it 'shows no leave project button if not part of project' do
-#      expect(page).to_not have_content('Leave Project')
-#    end
+    # hiwi can't leave projects, they can only be removed by wimis
+    it 'shows no leave project button' do
+      expect(page).to_not have_content(I18n.t('projects.show.leave_project'))
+    end
 
     it 'shows add working hours button' do
       @hiwi.projects << @project
       visit current_path
-      expect(page).to have_link('Add working hours')
+      expect(page).to have_link(I18n.t('projects.show.add_working_hours'))
     end
-
-    # hiwi can't see project if not a member
-#    it 'shows no add working hours button if not part of project' do
-#      expect(page).to_not have_link('Add working hours')
-#    end
 
     it 'shows working hours' do
       @hiwi.projects << @project
       visit current_path
-      expect(page).to have_content('Working hours')
+      expect(page).to have_content(I18n.t('projects.show.working_hours'))
     end
   end
 
@@ -83,21 +78,21 @@ RSpec.describe 'projects/show', type: :view do
       expect(page).to have_content(@project.title)
       expect(page).to have_content(@project.chair.name)
       expect(page).to have_content(l(@project.created_at))
-      expect(page).to_not have_content('public')
+      expect(page).to_not have_content(I18n.t('projects.form.show.public'))
     end
 
     it 'shows no add working hours button' do
       @wimi.projects << @project
       visit current_path
-      expect(page).to_not have_link('Add working hours')
+      expect(page).to_not have_link(I18n.t('projects.show.add_working_hours'))
     end
 
-    it 'shows a button to inspect a user specific working hour report for this project' do
+    it "shows a button to inspect a user's working hour report for this month and project" do
       user = FactoryGirl.create(:user)
       @wimi.projects << @project
       user.projects << @project
       visit current_path
-      expect(page).to have_selector(:link_or_button, 'Show working hours')
+      expect(page).to have_selector(:link_or_button, I18n.t('projects.form.show_current_working_hours'))
     end
 
     it 'shows one button to inspect all working hour report for this project' do
@@ -106,7 +101,7 @@ RSpec.describe 'projects/show', type: :view do
       user.projects << @project
       visit current_path
       expect(page).to have_content(I18n.t('projects.form.show_all_working_hours'), count: 1)
-      expect(page).to have_selector(:link_or_button, 'Show all working hours')
+      expect(page).to have_selector(:link_or_button, I18n.t('projects.form.show_all_working_hours'))
     end
     
     it 'not possible for wimi to edit if not a member' do
@@ -114,7 +109,6 @@ RSpec.describe 'projects/show', type: :view do
       expect(page).not_to have_selector(:link_or_button, I18n.t('helpers.links.edit'))
       expect(page).not_to have_selector(:link_or_button, I18n.t('helpers.links.destroy'))
       expect(page).not_to have_selector(:link_or_button, I18n.t('projects.show.set_inactive'))
-#      expect(page).to have_selector(:link_or_button, I18n.t('helpers.links.back'))
     end
   end
 end
