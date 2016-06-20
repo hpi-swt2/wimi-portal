de7816c63a42197b324ba211ac7731c1652ac2a6
Christoph Matthies <christoph.matthies@gmail.com>
2016-06-20 20:01:21 +0200

    another try for #400

--- db/migrate/20160525115010_add_contract_ref_to_time_sheets.rb
+++ db/migrate/20160525115010_add_contract_ref_to_time_sheets.rb
@@ -1,8 +1,8 @@
 class AddContractRefToTimeSheets < ActiveRecord::Migration
   def up
     add_reference :time_sheets, :contract, index: true, foreign_key: true
-    execute "UPDATE `time_sheets` SET `contract_id` = (SELECT MIN(c.`id`) FROM `contracts` c WHERE c.`hiwi_id` = `user_id`"
-    execute "DELETE FROM `time_sheets` WHERE `contract_id` IS NULL"
+    execute "UPDATE time_sheets SET contract_id = (SELECT MIN(c.id) FROM contracts c WHERE c.hiwi_id = user_id)"
+    execute "DELETE FROM time_sheets WHERE contract_id IS NULL"
     change_column_null :time_sheets, :contract_id, false
     remove_reference :time_sheets, :project
     remove_reference :time_sheets, :user
