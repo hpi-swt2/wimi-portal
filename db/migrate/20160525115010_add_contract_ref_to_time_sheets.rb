3e3adbd9ad545207fbe00c0ea9a0c3d1d8faa6b6
Christoph Matthies <christoph.matthies@gmail.com>
2016-06-20 19:42:06 +0200

    Another attempt at fixing the migration for PostgreSQL. #400

--- db/migrate/20160525115010_add_contract_ref_to_time_sheets.rb
+++ db/migrate/20160525115010_add_contract_ref_to_time_sheets.rb
@@ -1,17 +1,8 @@
 class AddContractRefToTimeSheets < ActiveRecord::Migration
   def up
     add_reference :time_sheets, :contract, index: true, foreign_key: true
-    TimeSheet.connection.execute <<-end_sql.squish
-      UPDATE 'time_sheets'
-      SET 'contract_id' = (
-          SELECT MIN(c.'id')
-          FROM 'contracts' c
-          WHERE c.'hiwi_id' = 'user_id'
-          )
-    end_sql
-    TimeSheet.connection.execute <<-end_sql.squish
-      DELETE FROM 'time_sheets' WHERE 'contract_id' IS NULL
-    end_sql
+    execute "UPDATE `time_sheets` SET `contract_id` = (SELECT MIN(c.`id`) FROM `contracts` c WHERE c.`hiwi_id` = `user_id`"
+    execute "DELETE FROM `time_sheets` WHERE `contract_id` IS NULL"
     change_column_null :time_sheets, :contract_id, false
     remove_reference :time_sheets, :project
     remove_reference :time_sheets, :user
