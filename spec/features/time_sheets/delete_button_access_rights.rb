ceee025ded6afaf553c39c77e5af7fd95e6acb13
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2016-10-16 01:19:05 +0200

    test(time sheet delete): Fix test for deletion rights on time sheets
    ref #472

--- spec/features/time_sheets/delete_button_access_rights.rb
+++ spec/features/time_sheets/delete_button_access_rights.rb
@@ -1,36 +1,30 @@
-# require 'rails_helper'
+require 'rails_helper'
 
 #this should work but it doesnt. feature works but test is broken. help please.
 
-# describe 'time_sheets#show' do
-#   before :each do
-#     @hiwi = FactoryGirl.create(:hiwi)
-#     @wimi = FactoryGirl.create(:wimi).user
-#     @contract = FactoryGirl.create(:contract, hiwi: @hiwi, responsible: @wimi)
-#     @project = FactoryGirl.create(:project)
-#     @project.users << @hiwi
-#     login_as @hiwi
-#   end
+describe 'time_sheets#show' do
+  before :each do
+    @hiwi = FactoryGirl.create(:hiwi)
+    @wimi = FactoryGirl.create(:wimi).user
+    @contract = FactoryGirl.create(:contract, hiwi: @hiwi, responsible: @wimi)
+    @time_sheet_new = FactoryGirl.create(:time_sheet, contract: @contract)
+    @time_sheet_handed_in = FactoryGirl.create(:time_sheet, contract: @contract, handed_in: true)
+    login_as @hiwi
+  end
 
-#   context 'with a new time sheet' do
-#     it 'has a delete button' do
-#       visit time_sheets_path
-#       click_on 'New Timesheet'
-#       click_on 'Create timesheet'
+  context 'with a new time sheet' do
+    it 'has a delete button' do
+      visit time_sheet_path(@time_sheet_new)
+      
+      expect(page).to have_content('Delete')
+    end
+  end
 
-#       expect(page).to have_content('Delete')
-#     end
-#   end
+  context 'with a handed in time sheet' do
+    it 'does not have a delete button' do
+      visit time_sheet_path(@time_sheet_handed_in)
 
-#   context 'with a handed in time sheet' do
-#     it 'does not have a delete button' do
-#       visit time_sheets_path
-#       click_on 'New Timesheet'
-#       click_on 'Create timesheet'
-#       click_on 'hand in'
-#       click_on 'show'
-
-#       expect(page).to_not have_content('Delete')
-#     end
-#   end
-# end
+      expect(page).to_not have_content('Delete')
+    end
+  end
+end
