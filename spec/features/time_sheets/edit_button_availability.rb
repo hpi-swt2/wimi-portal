2b4817ec86939062909c88299c376e68b171dfe6
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2016-10-23 22:42:23 +0200

    feat(timesheet#edit): Add placeholders to input fields
    also add missing test from last commit
    ref #483

---
+++ spec/features/time_sheets/edit_button_availability.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+#this should work but it doesnt. feature works but test is broken. help please.
+
+describe 'time sheets' do
+  before :each do
+    @hiwi = FactoryGirl.create(:hiwi)
+    @wimi = FactoryGirl.create(:wimi).user
+    @contract = FactoryGirl.create(:contract, hiwi: @hiwi, responsible: @wimi)
+    @time_sheet_new = FactoryGirl.create(:time_sheet, contract: @contract)
+    @time_sheet_handed_in = FactoryGirl.create(:time_sheet, contract: @contract, handed_in: true)
+    login_as @hiwi
+  end
+
+  context 'that are handed in' do
+    it 'cannot be edited' do
+      visit time_sheet_path(@time_sheet_handed_in)
+      
+      expect(page).not_to have_content('Edit')
+      hiwi_ability = Ability.new(@hiwi)
+      expect(hiwi_ability.can?(:edit, @time_sheet)).to be false
+    end
+  end
+
+  context 'that are not handed in' do
+    it 'can be edited' do
+      visit time_sheet_path(@time_sheet_new)
+
+      expect(page).to have_content('Edit')
+      hiwi_ability = Ability.new(@hiwi)
+      expect(hiwi_ability.can?(:edit, @time_sheet)).to be false
+    end
+  end
+end
