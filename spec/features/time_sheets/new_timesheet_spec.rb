bf2a47a9ac070ad85aae186341b592277c5ad177
Christoph Matthies <christoph.matthies@gmail.com>
2016-11-03 19:38:22 +0100

    Test(timesheet#new): Feature test creating timesheet

---
+++ spec/features/time_sheets/new_timesheet_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'Using time_sheets#new' do
+  before :each do
+    @hiwi = FactoryGirl.create(:hiwi)
+    @wimi = FactoryGirl.create(:wimi).user
+    @contract = FactoryGirl.create(:contract, hiwi: @hiwi, responsible: @wimi)
+    login_as @hiwi
+  end
+
+  it 'is possible to add a new work sheet' do
+    visit new_contract_time_sheet_path(@contract)
+
+    fill_in "time_sheet_month", with: Date.today.month
+    fill_in "time_sheet_year", with: Date.today.year
+
+    ts_count_before = TimeSheet.count
+    find('#hiddensubmit').click
+    expect(TimeSheet.count).to eq(ts_count_before+1)    
+    # No flash error
+    expect(page).to_not have_css('div.alert-danger')
+
+
+  end
+end
