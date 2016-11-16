98993aad8bd2f7966c2cc8c31e71b430067a932c
Christoph Matthies <christoph.matthies@gmail.com>
2016-11-16 17:24:57 +0100

    Test(time_sheet#new): Feature test for time sheets not in contract month. Ref #506

--- spec/features/time_sheets/new_timesheet_spec.rb
+++ spec/features/time_sheets/new_timesheet_spec.rb
@@ -6,18 +6,37 @@ describe 'Using time_sheets#new' do
     @wimi = FactoryGirl.create(:wimi).user
     @contract = FactoryGirl.create(:contract, hiwi: @hiwi, responsible: @wimi)
     login_as @hiwi
+    visit new_contract_time_sheet_path(@contract)
   end
 
-  it 'is possible to add a new work sheet' do
-    visit new_contract_time_sheet_path(@contract)
-
+  it 'is possible to add a new work sheet for current month' do
     fill_in "time_sheet_month", with: Date.today.month
     fill_in "time_sheet_year", with: Date.today.year
 
-    ts_count_before = TimeSheet.count
-    find('#hiddensubmit').click
-    expect(TimeSheet.count).to eq(ts_count_before+1)
+    expect { find('#hiddensubmit').click }.to change { TimeSheet.count }.by(1)
     # No flash error
     expect(page).to_not have_css('div.alert-danger')
+  end
+
+  it 'is not possible to add a work sheet for a month after contract end' do
+    after_contract_end = (@contract.end_date + 1.month).end_of_month
+
+    fill_in "time_sheet_month", with: after_contract_end.month
+    fill_in "time_sheet_year", with: after_contract_end.year
+    find('#hiddensubmit').click
+
+    expect(page).to have_css('div.alert-danger')
+    expect(page).to have_current_path(new_contract_time_sheet_path(@contract))
+  end
+
+  it 'is not possible to add a work sheet for a month before contract start' do
+    before_contract_start = (@contract.start_date - 1.month).beginning_of_month
+
+    fill_in "time_sheet_month", with: before_contract_start.month
+    fill_in "time_sheet_year", with: before_contract_start.year
+    find('#hiddensubmit').click
+
+    expect(page).to have_css('div.alert-danger')
+    expect(page).to have_current_path(new_contract_time_sheet_path(@contract))
   end
 end
