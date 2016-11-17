4cbc2fb584cbb934eb7a79813cc36960b610274f
Christoph Matthies <christoph.matthies@gmail.com>
2016-11-17 10:02:43 +0100

    Test(user feature): Refactor user feature tests. Unify 'profile' and 'user' dirs

--- spec/features/user/login_spec.rb
+++ spec/features/user/login_spec.rb
@@ -2,33 +2,26 @@ require 'rails_helper'
 
 feature 'Login' do
   before :each do
-    @routes = ['/projects']#, '/holidays', '/trips'
+    @routes = [projects_path, time_sheets_path, chairs_path, contracts_path] #'/holidays', '/trips'
   end
 
   it 'should not show any page as long as you are not logged in' do
     @routes.each do |route|
       visit route
-      expect(page).to have_content 'Please login first'
+      expect(page).to have_current_path(new_user_session_path)
+      expect(page).to have_css('div.alert-danger')
     end
   end
 
   context 'via OpenID' do
     it 'should be obligatory to insert a valid email before visiting all sites' do
-      @current_user = FactoryGirl.create(:user)
-      login_as @current_user
-      @current_user.update_attribute(:email, 'invalid_email')
+      current_user = FactoryGirl.create(:user)
+      login_as current_user
+      current_user.update_attribute(:email, 'invalid_email')
       @routes.each do |route|
         visit route
+        expect(page).to have_css('div.alert-danger')
         expect(page).to have_content 'Please set a valid email address first'
-      end
-
-      fill_in 'user_email', with: 'valid@e.mail'
-      click_on 'Save'
-
-      @routes.each do |route|
-        visit route
-        expect(page).to_not have_content 'Please set a valid email address first'
-        expect(page).to have_content 'Logout'
       end
     end
   end
@@ -44,33 +37,19 @@ feature 'Login' do
       fill_in 'user_password', with: @user.password
       click_on I18n.t('users.external_login.login')
 
-      expect(page).to_not have_content 'Please sign in'
+      expect(page).to_not have_css('div.alert-danger')
       @routes.each do |route|
         visit route
-        expect(page).to have_content 'Logout'
+        expect(page).to have_content I18n.t('helpers.application_tabs.logout')
       end
     end
-    
-    context 'with wrong credentials' do
       
-      it 'should not login a user' do
-        fill_in 'user_username', with: @user.username
-        fill_in 'user_password', with: @user.password + '_wrong'
-        click_on I18n.t('users.external_login.login')
-
-        @routes.each do |route|
-          visit route
-          expect(page).to_not have_content 'Logout'
-        end
-      end
-      
-      it 'should redirect to login page', exclude: true do
-        fill_in 'user_username', with: @user.username
-        fill_in 'user_password', with: @user.password + '_wrong'
-        click_on I18n.t('users.external_login.login')
-        
-        expect(current_path).to eq(external_login_path)
-      end
+    it 'should not login a user with wrong credentials' do
+      fill_in 'user_username', with: @user.username
+      fill_in 'user_password', with: @user.password + '_wrong'
+      click_on I18n.t('users.external_login.login')
+      expect(page).to have_css('div.alert-danger')
+      expect(page).to have_current_path(external_login_path)
     end
   end
 end
