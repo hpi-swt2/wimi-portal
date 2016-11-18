c6d2149032d6b4d726b4a500b233eb31306fc57b
Christoph Matthies <christoph.matthies@gmail.com>
2016-11-18 14:11:08 +0100

    Test(time_sheet): Refactor next_and_prev_month_spec.rb

--- spec/features/time_sheet/next_and_prev_month_spec.rb
+++ spec/features/time_sheet/next_and_prev_month_spec.rb
@@ -3,57 +3,49 @@ require 'rails_helper'
 describe 'time_sheet#show' do
   before :each do
     @user = FactoryGirl.create(:user)
-    start_date = Date.today << 1
-    end_date = Date.today >> 5
-    @contract = FactoryGirl.create(:contract, hiwi: @user, start_date: start_date, end_date: end_date)
-    @time_sheet = FactoryGirl.create(:time_sheet, contract: @contract, month: 12)
-    @time_sheet_next = FactoryGirl.create(:time_sheet, contract: @contract, month: @time_sheet.next_date[:month], year: @time_sheet.next_date[:year])
-    @time_sheet_prev = FactoryGirl.create(:time_sheet, contract: @contract, month: @time_sheet.previous_date[:month], year: @time_sheet.previous_date[:year])
+    today = Date.today
+    @start_date = (today - 1.month).beginning_of_month
+    @end_date = (today + 1.month).end_of_month
+    @contract = FactoryGirl.create(:contract, hiwi: @user, start_date: @start_date, end_date: @end_date)
+    @time_sheet = FactoryGirl.create(:time_sheet, contract: @contract, month: today.month)
     login_as @user
   end
 
-  it 'has a button for next page' do
-    visit time_sheet_path(@time_sheet)
+  context "previous and next month's time sheets are available" do
+    before :each do
+      @time_sheet_next = FactoryGirl.create(:time_sheet, contract: @contract, month: @end_date.month, year: @end_date.year)
+      @time_sheet_prev = FactoryGirl.create(:time_sheet, contract: @contract, month: @start_date.month, year: @start_date.year)
+    end
 
-    expect(page).to have_content(I18n.t('time_sheets.show.next_month'))
+    it "has a button to navigate to next month's time sheet" do
+      visit time_sheet_path(@time_sheet)
+      expect(page).to have_content(I18n.t('time_sheets.show.next_month'))
+      click_on I18n.t('time_sheets.show.next_month')
+      expect(current_path).to eq(time_sheet_path(@time_sheet_next))
+    end
 
-    click_on I18n.t('time_sheets.show.next_month')
-
-    expect(current_path).to eq(time_sheet_path(@time_sheet_next))
+    it "has a button to navigate to the previous month's time sheet" do
+      visit time_sheet_path(@time_sheet)
+      expect(page).to have_content(I18n.t('time_sheets.show.previous_month'))
+      click_on I18n.t('time_sheets.show.previous_month')
+      expect(current_path).to eq(time_sheet_path(@time_sheet_prev))
+    end
   end
 
-  it 'has a button for previous page' do
-    visit time_sheet_path(@time_sheet)
+  context "only the time sheet for the current month exists" do
+    it "has a button to create a sheet for next month if it doesn't exist yet" do
+      visit time_sheet_path(@time_sheet)
+      click_on I18n.t('time_sheets.show.create_next_month')
+      expect(current_path).to eq(new_contract_time_sheet_path(@contract))
+    end
 
-    expect(page).to have_content(I18n.t('time_sheets.show.previous_month'))
-
-    click_on I18n.t('time_sheets.show.previous_month')
-
-    expect(current_path).to eq(time_sheet_path(@time_sheet_prev))
+    it "has no button to create a sheet for next month if there is no contract for that month" do
+      @contract.end_date = @contract.start_date
+      @contract.save!
+      visit time_sheet_path(@time_sheet)
+      expect(page).not_to have_content(I18n.t('time_sheets.show.next_month'))
+      expect(page).not_to have_content(I18n.t('time_sheets.show.create_next_month'))
+    end
   end
 
-  it 'has a button to create a sheet for next month if it doesnt exist yet' do
-    @time_sheet_next.destroy
-
-    visit time_sheet_path(@time_sheet)
-
-    expect(page).to have_content(I18n.t('time_sheets.show.create_next_month'))
-
-    click_on I18n.t('time_sheets.show.create_next_month')
-
-    expect(page).to have_content("New")
-  end
-
-  it 'has no button to create a sheet for next month if there is no contract for that month' do
-    @time_sheet_next.destroy
-    @contract.destroy
-    start_date = Date.today << 1
-    end_date = Date.today >> 5
-    @contract = FactoryGirl.create(:contract, hiwi: @user, start_date: start_date, end_date: end_date)
-    @time_sheet.contract = @contract
-
-    visit time_sheet_path(@time_sheet)
-
-    expect(page).not_to have_content(I18n.t('time_sheets.show.create_next_month'))
-  end
 end
