d69682ea2493cf5fe644627f4d7d48f0e297e22a
Christoph Matthies <christoph.matthies@gmail.com>
2016-12-07 17:01:54 +0100

    Fix(dashboard): Add dashboard item showing contracts that end soon. Fixes #521

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,8 @@ class DashboardController < ApplicationController
     result = current_user.create_notification_arrays
     
     @activities = result[0]
-    @notifications = result[1]   
+    @notifications = result[1]
+
+    @ending_contracts = Contract.ends_soon.accessible_by(current_ability, :show)
   end
 end
