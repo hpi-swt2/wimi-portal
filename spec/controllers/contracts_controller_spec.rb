174605cf28ad76bc64c9a51f5c2ba16b5f0daa70
Christoph Matthies <christoph.matthies@gmail.com>
2017-01-10 11:59:14 +0100

    Refactor(contracts_controller_spec.rb): Use cancan matchers. Cleanup.

--- spec/controllers/contracts_controller_spec.rb
+++ spec/controllers/contracts_controller_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 require 'spec_helper'
+require 'cancan/matchers'
 
 RSpec.describe ContractsController, type: :controller do
   before(:each) do
     @user = FactoryGirl.create(:user)
     login_with @user
-    @project = FactoryGirl.create(:project)
-    @contract = FactoryGirl.create(:contract, hiwi: @user, chair: @project.chair, start_date: Date.new(2015,1), end_date: Date.new(2016,1))
-    @user_ability = Ability.new(@user)
+    @contract = FactoryGirl.create(:contract, hiwi: @user)
   end
 
   describe 'GET #dismiss' do
     it 'dismisses a month for a contract for the current user' do
-      expect(DismissedMissingTimesheet.dates_for(@user,@contract).size).to eq(0)
-      expect(@user_ability.can? :show, @contract).to be true
+      expect(DismissedMissingTimesheet.dates_for(@user, @contract).size).to eq(0)
+      expect(Ability.new @user).to be_able_to(:show, @contract)
 
       get :dismiss, {id: @contract.id, month: @contract.start_date.at_beginning_of_month}
       
       expect(flash[:success]).not_to be nil
-      expect(DismissedMissingTimesheet.dates_for(@user,@contract).size).to eq(1)
+      expect(flash[:error]).to be nil
+      expect(DismissedMissingTimesheet.dates_for(@user, @contract).size).to eq(1)
     end
   end
 end
