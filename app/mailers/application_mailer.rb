cf1699e3416612e8befac191522228bafebd7f12
Christoph Matthies <christoph.matthies@gmail.com>
2017-03-27 15:58:23 +0200

    Fix(ApplicationMailer): Remove settings from base class Custom settings are set in MailNotifier subclass.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   helper :application # gives access to all helpers defined within `application_helper`.
   include CanCan::ControllerAdditions
-
-  default from: 'HPI Wimi Portal <hpi.wimiportal@gmail.com>'
-  layout 'mailer'
 end
