9f9f0c3e36e8d472c850f8ede30cac8a633bcea5
Christoph Matthies <christoph.matthies@gmail.com>
2017-03-27 15:51:11 +0200

    Refactor(mail_notifier_spec): Use translations

--- app/mailers/mail_notifier.rb
+++ app/mailers/mail_notifier.rb
@@ -1,21 +1,15 @@
 class MailNotifier < ApplicationMailer
   default from: 'HPI Wimi Portal <hpi.wimiportal@gmail.com>'
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.mail_notifier.notification.subject
-  #
   def notification(event, user)
     l = I18n.locale
     I18n.locale = user.language
 
-    # i18n-tasks-use t'mail_notifier.notification.subject'
     @greeting = t('mail_notifier.notification.hello') + user.name
     @event = event
     @current_user = user
 
-    mail to: user.email
+    mail(to: user.email, subject: t('mail_notifier.notification.subject'))
     I18n.locale = l
   end
 
--- spec/mailers/mail_notifier_spec.rb
+++ spec/mailers/mail_notifier_spec.rb
@@ -2,24 +2,26 @@ require "rails_helper"
 
 RSpec.describe MailNotifier, type: :mailer do
   describe "notification" do
-    before(:each) do
-      @user = FactoryGirl.create(:user)
-      @event = FactoryGirl.create(:event, user: @user, target_user: @user)
-    end
-    let(:mail) { MailNotifier.notification(@event, @user) }
+
+    let(:user) { FactoryGirl.create(:user) }
+    let(:event) { FactoryGirl.create(:event, user: user, target_user: user) }
+    let(:mail) { MailNotifier.notification(event, user) }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("You received a new notification")
-      expect(mail.to).to eq([@user.email])
-      expect(mail.from).to eq(["hpi.wimiportal@gmail.com"])
+      expect(mail.subject).to eq(I18n.t("mail_notifier.notification.subject"))
+      expect(mail.to.count).to eq(1)
+      expect(mail.to.first).to eq(user.email)
+      expect(mail.from.count).to eq(1)
+      # Default from has the form '{display_name} <{email_address}>'
+      expect(MailNotifier.default[:from]).to include(mail.from.first)
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match('Hello')
+      expect(mail.body.encoded).to match(I18n.t("mail_notifier.notification.hello"))
     end
 
     it "finds name of user in email" do
-      expect(mail.body.encoded).to match(@user.name)
+      expect(mail.body.encoded).to match(user.name)
     end
   end
 
