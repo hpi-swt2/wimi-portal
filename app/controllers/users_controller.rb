83f60633bf40f87c9e7bc1fc30438fd950adb865
Christoph Matthies <christoph.matthies@gmail.com>
2017-03-28 16:12:32 +0200

    Feat(User#show): Latest activity for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,10 @@ class UsersController < ApplicationController
 
   def show
     @trips = @user.get_desc_sorted_trips
+    @recent_events = Event.where("user_id=? or target_user_id=?", @user.id, @user.id)
+      .limit(5)
+      .order(created_at: :desc)
+      .select { |e| current_ability.can?(:show, e) }
   end
 
   def edit
