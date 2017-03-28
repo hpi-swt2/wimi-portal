4046638ce731e515d5b8eb0af746aa2def00fff2
Christoph Matthies <christoph.matthies@gmail.com>
2017-03-28 12:11:15 +0200

    Refactor(MigrateEmailSettings): Allow rollback

--- db/migrate/20170323214831_migrate_email_settings.rb
+++ db/migrate/20170323214831_migrate_email_settings.rb
@@ -1,15 +1,35 @@
 class MigrateEmailSettings < ActiveRecord::Migration
 
+  def up
+    say_with_time "Setting full event_settings for all users who had notifications turned on" do
+      counter = 0
+      User.where(email_notification: true).find_each do |user|
+        user.update(event_settings: Event.types.values)
+        counter += 1
+      end
+      # If the result returned is an Integer, output includes a message
+      # about the number of rows in addition to the elapsed time.
+      counter
+    end
 
-	all_events = Event.types.collect { |type, val| val }
-
-	User.all.each do |user|
-		if user.email_notification
-			user.update(event_settings: all_events)
-		end
-	end
-
-  def change
-  	remove_column :users, :email_notification, :boolean
+    # Once existing data is migrated, remove email_notification column
+    remove_column :users, :email_notification, :boolean
   end
+
+  def down
+    # Readd email_notification column
+    add_column :users, :email_notification, :boolean
+
+    say_with_time "Filling recreated email_notification column" do
+      counter = 0
+      User.select { |u| u.event_settings.any? }.each do |user|
+        user.update(email_notification: true)
+        counter += 1
+      end
+      # If the result returned is an Integer, output includes a message
+      # about the number of rows in addition to the elapsed time.
+      counter
+    end
+  end
+
 end
