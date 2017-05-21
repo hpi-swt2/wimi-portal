7d7ce9c62c290f163785cbf5c76bea4fa40cb0ca
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2017-05-21 19:07:52 +0200

    chore(migration): migrate personnel number from 0 to nil

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170521142631) do
+ActiveRecord::Schema.define(version: 20170521170120) do
 
   create_table "chair_wimis", force: :cascade do |t|
     t.boolean "admin",          default: false
