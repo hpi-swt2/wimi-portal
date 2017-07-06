84a1e4945447e4281ce06c53842644763b618c12
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2017-07-06 15:23:53 +0200

    test(new timesheet button availability): add test for existence of button aswell
    ref #597

--- spec/features/time_sheet/new_button_availability_spec.rb
+++ spec/features/time_sheet/new_button_availability_spec.rb
@@ -2,52 +2,88 @@ require 'rails_helper'
 require 'cancan/matchers'
 
 describe 'dashboard#index' do
+  before :each do
+    @user = FactoryGirl.create(:user)
+    @contract = FactoryGirl.create(:contract, hiwi: @user)
+    login_as @user
+  end
+
   context 'for a hiwi without a project' do
     before :each do
-      @user = FactoryGirl.create(:user)
-      @contract = FactoryGirl.create(:contract, hiwi: @user)
       expect(@user.projects.count).to eq(0)
-      login_as @user
+      visit dashboard_path
     end
     
     it 'does not link to timesheet#current' do
-      visit dashboard_path
-      
       expect(page).not_to have_selector(:linkhref, current_time_sheets_path)
     end
     
     it 'does not link to timesheet#new' do
-      visit dashboard_path
-      
       expect(page).not_to have_selector(:linkhref, new_contract_time_sheet_path(@contract))
       within("div#contract") do
         expect(page).not_to have_selector(:submit)
       end
     end
   end
+
+  context 'for a hiwi with a contract and a project' do
+    before :each do
+      @project = FactoryGirl.create(:project, chair: @contract.chair)
+      @user.projects << @project
+      visit dashboard_path
+    end
+
+    it 'links to timesheet#current' do
+      expect(page).to have_selector(:linkhref, current_time_sheets_path)
+    end
+
+    it 'links to timesheet#new' do
+      within("div#contract") do
+        expect(page).to have_selector(:submit)
+      end
+    end
+  end
 end
 
 describe 'contracts#index' do
+  before :each do
+    @user = FactoryGirl.create(:user)
+    @contract = FactoryGirl.create(:contract, hiwi: @user)
+    login_as @user
+  end
+
   context 'for a hiwi without a project' do
     before :each do
-      @user = FactoryGirl.create(:user)
-      @contract = FactoryGirl.create(:contract, hiwi: @user)
       expect(@user.projects.count).to eq(0)
-      login_as @user
+      visit contract_path(@contract)
     end
     
     it 'does not link to timesheet#current' do
-      visit contract_path(@contract)
-      
       expect(page).not_to have_selector(:linkhref, current_time_sheets_path)
     end
     
     it 'does not link to timesheet#new' do
-      visit contract_path(@contract)
-      
       expect(page).not_to have_selector(:linkhref, new_contract_time_sheet_path(@contract))
       within("#timesheet_table") do
         expect(page).not_to have_selector(:submit)
+      end
+    end
+  end
+
+  context 'for a hiwi with a contract and a project' do
+    before :each do
+      @project = FactoryGirl.create(:project, chair: @contract.chair)
+      @user.projects << @project
+      visit contract_path(@contract)
+    end
+
+    it 'links to timesheet#new within sidebar' do
+      expect(page.find('#sidebar-content')).to have_selector(:linkhref, new_contract_time_sheet_path(@contract))
+    end
+
+    it 'links to timesheet#new within table' do
+      within("#timesheet_table") do
+        expect(page).to have_selector(:submit)
       end
     end
   end
@@ -64,5 +100,20 @@ describe 'ability for a user without a project' do
     ability = Ability.new(@user)
     
     expect(ability).to_not be_able_to(:new, TimeSheet)
+  end
+end
+
+describe 'ability for a user with a contract and a project' do
+  before :each do
+    @user = FactoryGirl.create(:user)
+    @contract = FactoryGirl.create(:contract, hiwi: @user)
+    @user.projects << FactoryGirl.create(:project, chair: @contract.chair)
+    expect(@user.projects.count).to eq(1)
+  end
+
+  it 'permits creating a new timesheet' do
+    ability = Ability.new(@user)
+    
+    expect(ability).to be_able_to(:new, TimeSheet)
   end
 end
