4d58a599af8fb7170250f3e86ff37c7e65e07299
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2017-08-29 19:23:25 +0200

    refactor(factories, test data): make add_demo_data use Factory Girl, expand factories
    work day factory now auto detects if there are other work days within the same time sheet already and chooses a date accordingly.
    
    demo data also creates a lot more data
    
    time sheet factory auto generates 3 work days
    the project of those work days can be set with the transient 'project' property

--- spec/factories/work_day_factory.rb
+++ spec/factories/work_day_factory.rb
@@ -17,13 +17,37 @@
 
 FactoryGirl.define do
   factory :work_day, class: 'WorkDay' do |f|
-    f.date Date.today
     f.start_time Time.now.middle_of_day
     f.break 0
     f.end_time Time.now.middle_of_day + 2.hours
     f.notes 'some note'
     f.time_sheet
-    f.project
+    f.project nil
     f.status ''
+    f.date { Date.new(time_sheet.year, time_sheet.month, 1)}
+    after(:build) do |wd, evaluator|
+      if wd.user.projects.empty?
+        project = FactoryGirl.create(:project, chair: wd.contract.chair)
+        project.users << wd.user
+      end
+      wd.project = wd.user.projects.first if wd.project == nil
+    end
+    after(:create) do |wd, evaluator|
+      wd.reload
+      unless wd.time_sheet.work_days.count < 2
+        max_date = wd.date
+        date_conflict = false
+        wd.time_sheet.work_days.each do |work_day|
+          if max_date < work_day.date
+            max_date = work_day.date
+          end
+          if wd.date == work_day.date and wd.id != work_day.id
+            date_conflict = true
+          end
+        end
+        wd.date = max_date + 1 if date_conflict and max_date < max_date.at_end_of_month
+        wd.save
+      end
+    end
   end
 end
