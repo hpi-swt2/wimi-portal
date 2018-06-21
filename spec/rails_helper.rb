cc8224a3eb19998d14488562da82eefcb1ab4e6e
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2018-06-21 08:58:39 +0200

    change deprecated Devise::TestHelper to Devise::Test::ControllerHelpers also change use_transactional_fixtures to false, since we are using database_cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,7 +36,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -52,8 +52,8 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
-  config.include Devise::TestHelpers, type: :controller
-  config.include Devise::TestHelpers, type: :view
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
   config.include Warden::Test::Helpers
   config.include FactoryGirl::Syntax::Methods
   config.include Capybara::DSL
